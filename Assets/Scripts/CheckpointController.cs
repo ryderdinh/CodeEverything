013d65d4874bd47ea26eb4e397af214ba9a30c29
ryderdinh <dinhquanganhdev@gmail.com>
2024-01-04 09:20:21 +0700

    update checkpoint

--- Assets/Scripts/CheckpointController.cs
+++ Assets/Scripts/CheckpointController.cs
@@ -3,15 +3,23 @@ using UnityEngine;
 public class CheckpointController : MonoBehaviour
 {
     public Transform respawnPoint;
+    public Sprite passive, active;
+    private Collider2D coll;
     private GameController gameController;
+
+    private SpriteRenderer spriteRenderer;
 
     private void Awake()
     {
         gameController = GameObject.FindGameObjectWithTag("Player").GetComponent<GameController>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        coll = GetComponent<Collider2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player")) gameController.UpdateCheckpoint(respawnPoint.position);
+        spriteRenderer.sprite = active;
+        coll.enabled = false;
     }
 }
